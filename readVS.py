80a51e3a365a414395aafa0254f0d8b25af1a1bd
laxentis <laxentis@gmail.com>
2021-03-18 14:54:01 +0100

    Initial commit

---
+++ readVS.py
@@ -0,0 +1,41 @@
+import urllib.request, json, re, csv
+
+with urllib.request.urlopen("https://data.vatsim.net/v3/vatsim-data.json") as url:
+    rawData = url.read().decode()
+    data = json.loads(rawData)
+
+writer = csv.writer(open('statistics.csv', 'a+', newline=''))
+
+time = data["general"]["update_timestamp"]
+entryfix = ['BILRA', 'BANUB', 'BINKA', 'PESEL', 'BODLA', 'ALUKA', 'XIGRI', 'SUBIX', 'ARSAP', 'GOVEN', 'POZUM', 'KORUP', 'LASIS', 'NAROX', 'RASAN', 'TOMTI',
+            'LAGAR', 'ELVOT', 'ENORU', 'DESEN', 'UTEVO', 'REGLI', 'BAVOK', 'PADKA', 'TUSIN', 'NETIR', 'SKARY', 'BABKO', 'MEBAN', 'SUPAK', 'KELEL', 'REGTO', 
+            'LENOV', 'PODAN', 'KEFIR', 'GAWOR', 'LUGOL', 'TEPNA', 'DIBED', 'GOTIX', 'ROLKA', 'USTIL', 'IVNER', 'TOLPA', 'BIGLU', 'TOSPO', 'LETKI', 'ABERO', 
+            'RUDKA', 'GORAT', 'ENOBI', 'SOTET', 'VABER', 'BOKSU', 'GOLAD', 'GITOV', 'GOMED', 'IPLIT', 'RANOK', 'KUNER', 'LATMI', 'IVGOR', 'VADRU', 'PENOR'
+            'LARMA', 'RUMAR', 'GORPI', 'LUSID', 'AMROR', 'KOLOB', 'GOSOT', 'POKEN']
+
+print(time)
+for pilot in data["pilots"]:
+    relevant = False
+    try:
+        fp = pilot["flight_plan"]
+        dep = fp["departure"]
+        matchDep = re.match(r'^EP..', dep, re.M|re.I)
+        if matchDep:
+            relevant = True
+        arr = fp["arrival"]
+        matchArr = re.match(r'^EP..', arr, re.M|re.I)
+        if matchArr:
+            relevant = True
+        altitude = fp["altitude"]
+        remarks = fp["remarks"]
+        matchRmk = re.match(r'EPWW', remarks, re.M|re.I)
+        if matchRmk:
+            relevant = True
+        route = fp["route"]
+        if any(fix in route for fix in entryfix):
+            relevant = True
+        if relevant:
+            print(dep, arr, altitude)
+            writer.writerow([time, dep, arr, altitude])
+    except (RuntimeError, TypeError, NameError):
+        pass
