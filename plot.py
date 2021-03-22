4f99aec17e81597cb80f6f475c3dcf96b5f428c5
laxentis <laxentis@gmail.com>
2021-03-22 09:48:17 +0100

    TQDM progress bar

--- plot.py
+++ plot.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 data = pd.read_csv("statistics.csv")
 
-for fl in range(285,375,10):
+for fl in tqdm(range(285,375,10)):
     plt.figure()
     plt.hist(data.Altitude / 100, bins=[0, fl, 660])
     plt.xlabel('Cruising Level [FL]')
